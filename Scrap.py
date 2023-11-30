68995bd057d2d90b11527e7c9f74aed38b1472ca
TheoBourderon <david.bourderon@gmail.com>
2023-11-30 20:27:34 +0100

    premier commit, update scrap.py et ajout de doc

--- Scrap.py
+++ Scrap.py
@@ -3,10 +3,29 @@ import requests
 from urllib.parse import quote
 
 key = os.getenv('key')
-name = quote("Sir Forty")
+
+name = quote("loubartheo")
 tag = "EUW"
 
-url = f"https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key={key}"
+url1 = f"https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key={key}"
 # url = f"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key={key}"
 
-print(requests.get(url).json())
+#print(requests.get(url1).json())
+
+puuid=requests.get(url1).json()['puuid']
+gametype= '420' #cf parameter.txt
+combien=10
+
+url2 = f"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?queue={gametype}&start=0&count={combien}&api_key={key}"
+
+#print(requests.get(url2).json())
+gamesId=requests.get(url2).json()
+game1=gamesId[0]
+
+url3= f"https://europe.api.riotgames.com/lol/match/v5/matches/{game1}?api_key={key}"
+
+print(requests.get(url3).json())
+
+
+
+
