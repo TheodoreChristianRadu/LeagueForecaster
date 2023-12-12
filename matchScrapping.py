6100c95dcae9840b08384cb82b6129a7fe7877ea
TheoBourderon <david.bourderon@gmail.com>
2023-12-12 20:50:02 +0100

    mis a jour de matchScrapping.py

--- matchScrapping.py
+++ matchScrapping.py
@@ -6,7 +6,7 @@ from urllib.parse import quote
 from time import sleep
 
 #key = os.getenv('key')
-key = f"RGAPI-6ac1d0a2-a899-4165-84a0-5956961c8c60"
+key = f"RGAPI-e5c4e2fc-d0ae-47d0-bfd8-c57250fc3f13"
 
 
 def get_match(matchId, max_retries=3, retry_delay=5):
@@ -36,46 +36,48 @@ def read_existing_log(log_file):
                     existing_summoners.add(matchId)
     return existing_summoners
 
-def process_csv(input_file, output_file, log_file, progress_interval=20):
-    used_matchId = read_existing_log(log_file)
+def process_csv(input_file, output_file, log_file, progress_interval=5):
+    used_historic = read_existing_log(log_file)
 
     with open(input_file, 'r') as csv_file:
         reader = csv.reader(csv_file)
         for i, row in enumerate(reader, 1):
-            matchIdList = row[0]
+            matchHistoricList = row
 
             # Vérifiez si le summoner a déjà été traité
-            if matchIdList in used_matchId:
-                print(f"{id} a déjà été traité. Ignoré.")
+            if ''.join(matchHistoricList) in used_historic:
+                print(f"Déjà été traité.")
                 continue
+            
+            matchList =[]
+            for id in matchHistoricList:
+                if id != '':
+                    sleep_time = 0.5
+                    sleep(sleep_time)
+                    match = get_match(id) #a corriger
+                    if match:
+                        matchList.append(json.dumps(match))
 
-            for id in matchIdList:
-                historic = get_match(id) #a corriger
-                if historic:
+            # Enregistrez le progrès dans le fichier de journalisation
+            with open(log_file, 'a') as log:
+                log.write(f" Traitement réussi pour {''.join(matchHistoricList)}\n")
 
-                    # Enregistrez le progrès dans le fichier de journalisation
-                    with open(log_file, 'a') as log:
-                        log.write(f" Traitement réussi pour {id}\n")
+            # Affichez le progrès tous les progress_interval summoners
+            if i % progress_interval == 0:
+                print(f"\nTraitement réussi pour {i} summoners.\n")
 
-                    # Affichez le progrès tous les progress_interval summoners
-                    if i % progress_interval == 0:
-                        print(f"\nTraitement réussi pour {i} summoners.\n")
+            with open(output_file, 'w') as fichier_json:
+                # Utilisation de json.dump pour écrire chaque ligne dans le fichier
+                json.dump(matchList, fichier_json)
+                fichier_json.write('\n')
 
-                    # Écrivez dans le fichier de sortie à chaque itération
-                    with open(output_file, mode='a', newline='') as result_csv:
-                        writer = csv.writer(result_csv)
-                        writer.writerows([list(historic)])
-                        # Ajoutez la possibilité de faire des pauses entre les exécutions successives
-                        # pour respecter les limitations de taux de l'API.
-                        sleep_time = 0.5
-                        sleep(sleep_time)
 
     print("Toutes les requêtes ont été traitées avec succès.")
 
 if __name__ == "__main__":
-    input_csv_path = 'puuidIdList.csv'
-    output_csv_path = 'historicList.csv'
-    log_file_path = 'executionHistoric_log.txt'
+    input_csv_path = 'historicList1.csv'
+    output_csv_path = 'matchList.json'
+    log_file_path = 'executionMatch_log.txt'
 
     try:
         print("Début du traitement...")
